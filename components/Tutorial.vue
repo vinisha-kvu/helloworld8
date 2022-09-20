e754ae0249da16d6cd8576be60b2aeace037df49
vinisha.kalevaru <vinisha.kalevaru@diva-e.com>
2022-09-20 15:13:22 +0200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1>Hello World </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
